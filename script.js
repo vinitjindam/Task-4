e7c84588d1ad412906effe84efe7aba9480cc1a0
vinitjindam75 <69238936+vinitjindam@users.noreply.github.com>
2021-10-02 20:02:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function initMap(){
+    const loc={lat:26.361145,lng:-75.45};
+    const map=new google.maps.Map(document.querySelector('.map')
+    ,{
+        zoom:14,
+        center:loc
+    
+});
+const marker= new google.maps.Marker({position:loc,map:map});
+}
+
+
+$('#navbar a,.btn').on('click',function(event){
+    if(this.hash !==''){
+        event.preventDefault();
+
+        const hash=this.hash;
+        $('html','body').animate(
+            {
+                scrollTop: $(hash).offset().top-100
+            },
+            800
+        );
+    }
+});
